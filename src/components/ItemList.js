ce71a4b5110203327c593171d0393dbc6a7224fb
Henry Gordon <hwgordon247@gmail.com>
2017-09-03 17:21:22 +0100

    creation of link to profile page from an item

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -7,7 +7,14 @@ class ItemList extends React.Component {
         <div key={index} className="item">
           <div>{item.name}</div>
           <div>{item.description}</div>
-          <div>Owned By: {item.ownedBy.username}</div>
+          <div>
+            <span>
+              Owned By:
+            </span>
+            <a className="username" href={`profile/${item.ownedBy.username}`}>
+              {item.ownedBy.username}
+            </a>
+          </div>
         </div>
       );
     });
